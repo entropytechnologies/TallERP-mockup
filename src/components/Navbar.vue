0eae62780eca671dd564bf9e0d7e270bda2b0336
albbarrera96 <85797748+albbarrera96@users.noreply.github.com>
2023-01-11 16:58:29 -0500

    Navbar added

---
+++ src/components/Navbar.vue
@@ -0,0 +1,42 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <router-link class="nav-link" aria-current="page" to="/home">Home</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link class="nav-link" aria-current="page" to="/customers">Customers</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link class="nav-link" aria-current="page" to="/vehicles">Vehicles</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link class="nav-link" aria-current="page" to="/sales">Sales</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link class="nav-link" aria-current="page" to="/service">Service</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link class="nav-link" aria-current="page" to="/tirestorage">Tire Storage</router-link>
+                </li>
+            </ul>
+            </div>
+        </div>
+    </nav>    
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
