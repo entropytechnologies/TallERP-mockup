d8af7a35d4c4d09fec366e151296f8afce474c05
albbarrera96 <85797748+albbarrera96@users.noreply.github.com>
2023-02-27 12:08:52 -0500

    Preview updated

--- src/views/ImportData.vue
+++ src/views/ImportData.vue
@@ -36,24 +36,83 @@
                         <div class="row mt-4"> 
                             <div class="col-12">
                                 <h4 class="card-title">Field Mapping</h4>
-                                <div class="row mt-4">
-                                    <div class="col-6">
-                                        <h5 class="text-center mb-4">Uploaded File fields</h5>
-                                        <div v-for="(newKey, index) in newKeys" class="d-flex">  
-                                            <select class="form-select me-3" id="">
-                                                <option selected>{{newKey}}</option>
-                                                <option :value="newKey" v-for="(newKey, index) in fileKeys"> {{newKey}}</option>
-                                            </select>
+                                <!---Tabs-->
+                                <ul class="nav nav-tabs justify-content-center mt-4" id="myTab" role="tablist">
+                                    <li class="nav-item" role="presentation">
+                                        <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#fieldMapping-tab-pane" type="button" role="tab" aria-controls="fieldMapping-tab-pane" aria-selected="true">
+                                            <i class="fa-solid fa-table "></i>
+                                            Field Mapping
+                                        </button>
+                                    </li>
+                                    <li class="nav-item" role="presentation">
+                                        <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#preview-tab-pane" type="button" role="tab" aria-controls="preview-tab-pane" aria-selected="false">
+                                            <i class="fa-solid fa-magnifying-glass me-1"></i>
+                                            Preview
+                                        </button>
+                                    </li>
+                                </ul>
+                                <!---Tabs-->
+                                <!---Tabs Content-->
+                                <div class="tab-content" id="myTabContent">
+                                    <div class="tab-pane fade show active" id="fieldMapping-tab-pane" role="tabpanel" aria-labelledby="fieldMapping-tab" tabindex="0">
+                                        <div class="row mt-4">
+                                            <div class="col-6">
+                                                <h5 class="text-center mb-4">Uploaded File fields</h5>
+                                                <div v-for="(newKey, index) in newKeys" class="d-flex">  
+                                                    <select class="form-select me-3" id="">
+                                                        <option selected>{{newKey}}</option>
+                                                        <option :value="newKey" v-for="(newKey, index) in fileKeys"> {{newKey}}</option>
+                                                    </select>
+                                                </div>
+                                            </div> 
+                                            <div class="col-6">
+                                                <h5 class="text-center mb-4">System fields</h5>
+                                                <select class="form-select" id="" v-for="(customerField, index) in customerFields">
+                                                    <option selected>{{ customerField }}</option>
+                                                    <option value="" v-for="(customerField, index) in customerFields">{{ customerField }}</option>
+                                                </select> 
+                                            </div>
                                         </div>
-                                    </div> 
-                                    <div class="col-6">
-                                        <h5 class="text-center mb-4">System fields</h5>
-                                        <select class="form-select" id="" v-for="(customerField, index) in customerFields">
-                                            <option selected>{{ customerField }}</option>
-                                            <option value="" v-for="(customerField, index) in customerFields">{{ customerField }}</option>
-                                        </select> 
+                                    </div>
+                                    <div class="tab-pane fade" id="preview-tab-pane" role="tabpanel" aria-labelledby="preview-tab" tabindex="0">
+                                        <div class="container">
+                                            <div class="" v-if="fileKeys != [] && fileContent != []">
+                                                <h5 class="text-center mt-4">Uploaded File</h5>
+                                                <table class="table table-striped">
+                                                    <thead>
+                                                        <tr>
+                                                            <th v-for="(fileKey, index) in fileKeys" scope="col">{{fileKey}}</th>
+                                                        </tr>    
+                                                    </thead>
+                                                    <tbody>
+                                                        <tr v-for="(fileContent, index) in fileContent">
+                                                            <th scope="row">{{index}}</th>
+                                                            <td class="text-truncate" v-for="(fileKey, index) in fileKeys">{{fileContent[fileKey]}}</td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                                <hr>
+                                                <h5 class="text-center mt-4">System's Data</h5>
+                                                <div class="table-responsive">
+                                                    <table class="table table-striped">
+                                                        <thead>
+                                                            <tr>
+                                                                <th v-for="(customerField, index) in customerFields" scope="col">{{customerField}}</th>
+                                                            </tr>    
+                                                        </thead>
+                                                        <tbody>
+                                                            <tr v-for="(customer, index) in customers">
+                                                                <th scope="row">{{index}}</th>
+                                                                <td class="text-truncate" v-for="(customerField, index) in customerFields">{{customer[customerField]}}</td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
+                                <!---Tabs Content-->
                             </div>
                             <div class="text-center mt-2">
                                 <!-- Button trigger modal -->
@@ -62,67 +121,10 @@
                                     Save Changes
                                 </button>
                                 <!-- Button trigger modal -->
-                                <button type="button" class="btn btn-info my-3 text-center me-2" data-bs-toggle="modal" data-bs-target="#previewModal">
-                                    <i class="fa-solid fa-magnifying-glass me-1"></i>
-                                    Preview your data
-                                </button>
-                                <!-- Button trigger modal -->
-                                <button type="button" class="btn btn-warning my-3 text-center"  @click="clearData">
+                                <button type="button" class="btn btn-warning my-3 text-center" @click="clearData">
                                     <i class="fa-solid fa-delete-left me-1"></i>
                                     Clear data
                                 </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div v-if="dataHandling">
-                        <!-- Modal -->
-                        <div class="modal fade" id="previewModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div class="modal-dialog modal-xl">
-                                <div class="modal-content">
-                                <div class="modal-header">
-                                    <h1 class="modal-title fs-4" id="exampleModalLabel">Preview</h1>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    <h5 class="text-center">Uploaded File</h5>
-                                    <table class="table table-striped">
-                                        <thead>
-                                            <tr>
-                                                <th>#</th>
-                                                <th v-for="(fileKey, index) in fileKeys" scope="col">{{fileKey}}</th>
-                                            </tr>    
-                                        </thead>
-                                        <tbody>
-                                            <tr v-for="(fileContent, index) in fileContent">
-                                                <th scope="row">{{index}}</th>
-                                                <td class="text-truncate" v-for="(fileKey, index) in fileKeys">{{fileContent[fileKey]}}</td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                    <hr>
-                                    <h5 class="text-center mt-4">System's Data</h5>
-                                    <div class="table-responsive">
-                                        <table class="table table-striped">
-                                            <thead>
-                                                <tr>
-                                                    <th>#</th>
-                                                    <th v-for="(customerField, index) in customerFields" scope="col">{{customerField}}</th>
-                                                </tr>    
-                                            </thead>
-                                            <tbody>
-                                                <tr v-for="(customer, index) in customers">
-                                                    <th scope="row">{{index}}</th>
-                                                    <td class="text-truncate" v-for="(customerField, index) in customerFields">{{customer[customerField]}}</td>
-                                                </tr>
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                    <button type="button" class="btn btn-primary">Save changes</button>
-                                </div>
-                                </div>
                             </div>
                         </div>
                     </div>
@@ -134,7 +136,6 @@
 </template>
 
 <script>
-import { toUpper } from 'lodash';
 import {ref} from 'vue'
 import customers from '../assets/js/customersData.json'
 import customerFields from '../assets/js/customersFields.json'
@@ -164,12 +165,12 @@ export default {
         };
         const clearData = () => {
             fileContent.value = '';
+            fileKeys.value = [];
             newKeys.value = [];
         };
         const updateKeys = () => {
-            fileKeys.value === newKeys.value;
+            fileKeys.value = newKeys.value;
         };
-
         customers
         customerFields
 
@@ -179,6 +180,7 @@ export default {
             dataHandling,
             fileKeys,
             newKeys,
+
             toggleImportType,
             uploadConfirm,
             clearData,
